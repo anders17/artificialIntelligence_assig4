2092fc17f2f303f2061e5bb65304e436340859cd
Augusto Wong <augustowong2010@hotmail.com>
2018-04-08 16:38:12 -0400

    Update classes

--- Node.py
+++ Node.py
@@ -5,4 +5,8 @@ class Node:
         self.isStart = False
         self.isPit = False
         self.isWall = False
+        self.reward = -1
+        #0=UP, 1=Right, 2=Down, 3= Left, 4= Give Up
+        self.possRewards = [0] * 5
+        self.bestAction = "^"
 
