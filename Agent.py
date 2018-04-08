fe3f5bd55e701c9e342db303697f434a2e4dced5
Andrew Santos <andrewsantos@rasantos.wifi.wpi.edu>
2018-04-08 16:39:18 -0400

    Agent functions

--- Agent.py
+++ Agent.py
@@ -1,4 +1,6 @@
 #Agent class
+import random
+
 class Agent:
     def __init__(self):
         #Dictionary
@@ -6,18 +8,23 @@ class Agent:
         self.Q_pairs = {} # key is [statex,statey,action]
 
 
-    # choose move
+    # choose action
     # see if exploring
     # if not, determine move to take based on q(s,a) values
     # or give up
-    def choose_move(self,world,epsilon):
-        pass
+    def choose_action(self,epsilon):
+        chosen_action = None
+        if random.randint(0,100) < epsilon:
+            pass
+        else:
+            pass
+        return chosen_action
 
 
     # function to determine the move actually taken
     # given move desired, 10% perpendicular each direction, 10% 2x forward
     # outputs action
-    def make_move(self,action):
+    def make_action(self,action):
         pass
 
 
