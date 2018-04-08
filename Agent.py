9bad62004e2c882c298c9330f364871308673534
Augusto Wong <augustowong2010@hotmail.com>
2018-04-08 17:59:28 -0400

    minor changes

--- Agent.py
+++ Agent.py
@@ -34,41 +34,20 @@ class Agent:
         pass
 
 
-    #Next coordinates if you take this action
-    def nextCoord(self, currState, action):
-        i = currState[0]
-        j = currState[1]
-
-        #Up
-        if(action == 0):
-          j+=1
-        #Right
-        elif(action == 1):
-          i+=1
-        #Down
-        elif(action == 2):
-          j-=1
-
-        #Left
-        elif(action == 3):
-          i-=1
-
-        return [i,j]
-
-
-
     # calculate q(s,a) based on current_state and action to be taken
     # append current_state
-    def updateQ(self,world,currState, nextState, action1,action2):
+    def updateQ(self,world, currState, nextState, action1,action2):
+        #q1
         i = currState[0]
         j = currState[1]
         q1 = world.grid[i][j].possRewards[action1]
 
-
+        #q2
         i2 = nextState[0]
         j2 = nextState[1]
         q2 = world.grid[i2][j2].possRewards[action2]
 
+        #Math to update q1 based on what we found on q2
         math = q1 + self.stepSize * (world.grid[i][j].reward + (self.gama * q2) - q1)
 
         #Update q1
@@ -96,6 +75,28 @@ class Agent:
                     break
 
         world.printRecActions()
+
+
+    #Next coordinates if you take this action
+    # def nextCoord(self, currState, action):
+    #     i = currState[0]
+    #     j = currState[1]
+    #
+    #     #Up
+    #     if(action == 0):
+    #       j+=1
+    #     #Right
+    #     elif(action == 1):
+    #       i+=1
+    #     #Down
+    #     elif(action == 2):
+    #       j-=1
+    #
+    #     #Left
+    #     elif(action == 3):
+    #       i-=1
+    #
+    #     return [i,j]
 
 
 
