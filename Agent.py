2f2c09b62557465c0864d332e5cdd1f7f3cdb86c
Augusto Wong <augustowong2010@hotmail.com>
2018-04-08 17:53:48 -0400

    working on update Q

--- Agent.py
+++ Agent.py
@@ -7,6 +7,9 @@ class Agent:
         self.current_state = [] # example: [1,0]
         self.moveReward = moveReward
         self.giveUpReward = giveUpReward
+        self.possibleMoves = 5
+        self.stepSize = 0.5
+        self.gama = 0.9
 
     # choose action
     # see if exploring
@@ -29,10 +32,48 @@ class Agent:
         pass
 
 
+    #Next coordinates if you take this action
+    def nextCoord(self, currState, action):
+        i = currState[0]
+        j = currState[1]
+
+        #Up
+        if(action == 0):
+          j+=1
+        #Right
+        elif(action == 1):
+          i+=1
+        #Down
+        elif(action == 2):
+          j-=1
+
+        #Left
+        elif(action == 3):
+          i-=1
+
+        return [i,j]
+
+
+
     # calculate q(s,a) based on current_state and action to be taken
     # append current_state
-    def updateQ(self,world,action):
-        pass
+    def updateQ(self,world,currState, nextState, action1,action2):
+        i = currState[0]
+        j = currState[1]
+        q1 = world.grid[i][j].possRewards[action1]
+
+
+        i2 = nextState[0]
+        j2 = nextState[1]
+        q2 = world.grid[i2][j2].possRewards[action2]
+
+        math = q1 + self.stepSize * (world.grid[i][j].reward + (self.gama * q2) - q1)
+
+        #Update q1
+        world.grid[i][j].possRewards[action1] = math
+
+
+
 
     def train(self,trailNum,epsilon):
         pass
--- Node.py
+++ Node.py
@@ -11,6 +11,7 @@ class Node:
 
         self.isWall = False
         self.reward = -1
+
         #0=UP, 1=Right, 2=Down, 3= Left, 4= Give Up
         self.possRewards = [0] * 5
         self.bestAction = 0
