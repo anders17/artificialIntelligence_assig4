ef87a58c820083ea8b44d68c4a08f23d8e94c012
Augusto Wong <augustowong2010@hotmail.com>
2018-04-09 18:20:21 -0400

    implemented make_action

--- Agent.py
+++ Agent.py
@@ -29,13 +29,136 @@ class Agent:
                     chosen_action = i
         return chosen_action
 
+    #returns true if it is a valid I position (based on the width of the world)
+    def validI(self, i, world):
+        width = world.width
 
+        #Check if withing boundaries and not a wall
+        if(i < width-1 and i > 0):
+            return True
+
+        return False
+
+
+    #returns true if it is a valid J position (based on the height of the world)
+    def validJ(self, j, world):
+        height = world.height
+
+        #Check if within boundaries
+        if(j < height-1 and j>0):
+            return True
+
+        return False
+
+    #returns true if this coordinate will not fall in the pit or hit a wall
+    def isThisPit(self,i,j,world):
+        return world.grid[i][j].isPit
 
     # function to determine the move actually taken
     # given move desired, 10% perpendicular each direction, 10% 2x forward
     # outputs 1 if found pit, goal, or decided to give up; otherwise return 0
-    def make_action(self,action):
-        pass
+    def make_action(self,action, world):
+        i = self.current_state[0]
+        j = self.current_state[1]
+
+        myRandom = random.randint(0, 9)
+
+        #Normal Behavior 70%
+        if(myRandom <= 6):
+            #Up
+            if(action == 0 and self.validJ(j+1, world)):
+              j+=1
+
+            #Right
+            elif(action == 1 and self.validI(i+1, world)):
+              i+=1
+
+            #Down
+            elif(action == 2 and self.validJ(j-1, world)):
+              j-=1
+
+            #Left
+            elif(action == 3 and self.validI(i-1, world)):
+              i-=1
+
+        #perpendicular Left 10%
+        elif(myRandom == 7):
+            #Up
+            if(action == 0 and self.validI(i-1, world)):
+              #LEFT
+              i-=1
+
+            #Right
+            elif(action == 1 and self.validJ(j+1, world) ):
+              #UP
+              j+=1
+
+            #Down
+            elif(action == 2 and self.validI(i+1, world)):
+              #RIGHT
+              i+=1
+
+            #Left
+            elif(action == 3 and self.validJ(j-1, world)):
+              #DOWN
+              j-=1
+
+        #Perpendicular Right 10%
+        elif(myRandom == 8):
+            #Up
+            if(action == 0 and self.validI(i+1, world)):
+              #RIGHT
+              i+=1
+
+            #Right
+            elif(action == 1 and self.validJ(j-1, world)):
+              #DOWN
+              j-=1
+
+            #Down
+            elif(action == 2 and self.validI(i-1, world)):
+              #LEFT
+              i-=1
+
+            #Left
+            elif(action == 3 and self.validJ(j+1, world)):
+              #UP
+              j+=1
+
+
+
+        #Double step 10%
+        #NOTE: check every step
+        elif(myRandom == 9):
+            extraSteps = 2
+            for i in xrange(extraSteps):
+                #Up
+                if(action == 0 and self.validJ(j+1, world)):
+                  j+=1
+
+                #Right
+                elif(action == 1 and self.validI(i+1, world)):
+                  i+=1
+
+                #Down
+                elif(action == 2 and self.validJ(j-1, world)):
+                  j-=1
+
+                #Left
+                elif(action == 3 and self.validI(i-1, world)):
+                  i-=1
+
+                #If the new coordinates happen to be in the pit then just return them
+                if(self.isThisPit(i,j,world)):
+                    return [i,j]
+
+                #otherwise you can keep changing them
+
+        #Return the valid coordinates
+        return [i,j]
+
+
+
 
 
     # calculate q(s,a) based on current_state and action to be taken
